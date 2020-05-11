e87a386ce0ec58b41b926b782467ad42fb714132
Angel Rosmend <60056902+angelrosmend@users.noreply.github.com>
2020-05-10 23:48:15 -0300

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,15 +9,15 @@ function Nav() {
   <nav>
    <h3 className="logo">Social App</h3>
    <ul className="nav-links">
-    <Link className="link" to={"/home/perfil"}>
-    <li>Perfil</li>
-    </Link>
-    <Link className="link"  to="/home/amigos">
-    <li>Amigos</li>
-    </Link>
-    <Link className="link" to="/landingpage/registro">
-    <li>Cerrar sesión</li>
-    </Link>
+       <Link className="link" to={"/home/perfil"}>
+          <li>Perfil</li>
+       </Link>
+       <Link className="link"  to="/home/amigos">
+          <li>Amigos</li>
+       </Link>
+       <Link className="link" to="/landingpage/registro">
+          <li>Cerrar sesión</li>
+       </Link>
    </ul> 
   </nav>
  )
